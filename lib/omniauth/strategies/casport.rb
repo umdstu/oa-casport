7973d3282786add45501942bf9523f656a966103
capollo-local <capollo-local@rd6um-86337h.infosec.tycho.ncsc.mil>
2013-02-12 13:15:59 -0500

    updated to omniauth 1.x syntax

--- lib/omniauth/strategies/casport.rb
+++ lib/omniauth/strategies/casport.rb
@@ -1,4 +1,4 @@
-require 'omniauth/core'
+require 'omniauth'
 require 'httparty'
 require 'redis'
 require 'uri'
@@ -31,118 +31,106 @@ module OmniAuth
       include OmniAuth::Strategy
       include HTTParty
       
-      def initialize(app, options)
-        super(app, :casport)
-        @options = options
-        @options[:cas_server]    ||= 'http://cas.dev/users'
-        @options[:format]        ||= 'xml'
-        @options[:format_header] ||= 'application/xml'
+      args[:setup, :ssl_ca_file, :pem_cert, :cert_password, :cas_server]
+
+      option :name,         'casport'
+      option :dn,           nil
+      option :full_name,    nil
+      option :last_name,    nil
+      option :uid,          nil
+      option :first_name,   ""
+      option :display_name, ""
+      option :title,        ""
+      option :permission,   ""
+      option :email,        ""
+      option :uid_field,    :uid
+
+
+      uid do {
+        user['dn']
+      } end
+
+      info do {
+        :name => user['full_name']
+        :email => user['email']
+      } end
+
+      extra do {
+        'raw_info' => raw_info
+      } end
+
+      def raw_info
+        user
       end
 
+
+
       def request_phase
-        # Can't get user data without their UID for the CASPORT server 
-        raise "No UID set in request.env['omniauth.strategy'].options[:uid]" if @options[:uid].nil?
-        Casport.setup_httparty(@options)
-        redirect(callback_path)
+        if options.uid.nil? || options.uid.empty?
+          e = "No UID set in request.env['omniauth.strategy'].options[:uid]"
+          fail(!uid_not_found, e)
+          raise e
+        end
+
+        Casport.setup_httparty(options)
+        redirect("#{OmniAuth.config.full_host}#{callback_path}")
       end
-    
+
       def callback_phase
         begin
-          raise 'We seemed to have misplaced your credentials... O_o' if user.nil?
           super
         rescue => e
           redirect(request_path)
-#          fail!(:invalid_credentials, e)
+          fail(:invalid_credentials, e)
         end
-        call_app!
+
+        super
       end
 
-      def auth_hash
-        # store user in a local var to avoid new method calls for each attribute
-        user_obj = user
-        begin
-          # convert all Java camelCase keys to Ruby snake_case, it just feels right!
-          user_obj = user_obj['userinfo'].inject({}){|memo, (k,v)| memo[k.gsub(/[A-Z]/){|c| '_'+c.downcase}] = v; memo}
-        rescue => e
-          fail!(:invalid_user, e)
-        end
-        OmniAuth::Utils.deep_merge(super, {
-          'uid'       => user_obj['dn'],
-          'user_info' => {
-                          'name' => user_obj['full_name'],
-                          'email' => user_obj['email']
-                         },
-          'extra'     => {'user_hash' => user_obj}
-        })
-      end
-
-      # Set HTTParty params that we need to set after initialize is called
-      # These params come from @options within initialize and include the following:
-      # :ssl_ca_file - SSL CA File for SSL connections
-      # :format - 'json', 'xml', 'html', etc. || Defaults to 'xml'
-      # :format_header - :format Header string || Defaults to 'application/xml'
-      # :pem_cert - /path/to/a/pem_formatted_certificate.pem for SSL connections
-      # :pem_cert_pass - plaintext password, not recommended!
       def self.setup_httparty(opts)
         format opts[:format].to_sym
-        headers 'Accept'               => opts[:format_header]
-        headers 'Content-Type'         => opts[:format_header]
-        headers 'X-XSRF-UseProtection' => 'false' if opts[:format] == 'json'
-        if opts[:ssl_ca_file]
-          ssl_ca_file opts[:ssl_ca_file]
-          if opts[:pem_cert_pass]
-            pem File.read(opts[:pem_cert]), opts[:pem_cert_pass]
+        headers 'Accept'               => opts.format_header
+        headers 'Content-Type'         => opts.format_header
+        headers 'X-XSRF-UseProtection' => 'false' if opts.format == 'json'
+        if opts.ssl_ca_file
+          ssl_ca_file opts.ssl_ca_file
+          if opts.pem_cert_pass
+            pem File.read(opts.pem_cert), opts.pem_cert_pass
           else
-            pem File.read(opts[:pem_cert])
+            pem File.read(opts.pem_cert)
           end
         end
       end
       
       def user
-        # Can't get user data without a UID from the application
-        begin
-          raise "No UID set in request.env['omniauth.strategy'].options[:uid]" if @options[:uid].nil?
-          # Fix DN order (if we have a DN) for CASPORT to work properly
-          if @options[:uid].include?('/') or @options[:uid].include?(',')
-            # Convert '/' to ',' and split on ','
-            @options[:uid] = @options[:uid].gsub('/',',').split(',').reject{|array| array.all? {|el| el.nil? || el.strip.empty? }}
-            # See if the DN is in the order CASPORT expects (and fix it if needed)
-            @options[:uid] = @options[:uid].reverse if @options[:uid].first.downcase.include? 'c='
-            # Join our array of DN elements back together with a comma as expected by CASPORT
-            @options[:uid] = @options.join ','
-          end
-        rescue => e
-          fail!(:uid_not_found, e)
+        
+        if options.uid.include?('/') or options.uid.include?(',')
+          options.uid = options.uid.gsub('/',',').split(',').reject{|array| array.all? {|el| el.nil? || el.strip.empty? }}
+          options.uid = options.uid.reverse if @options[:uid].first.downcase.include? 'c='
+          options.uid = options.join ','
         end
-
+      
         begin
-          raise Errno::ECONNREFUSED if @options[:redis_options] == 'disabled'
-          cache = @options[:redis_options].nil? ? Redis.new : Redis.new(@options[:redis_options])
-          unless @user = (cache.get @options[:uid])
-            # User is not in the cache
-            # Retrieving the user data from CASPORT
-            # {'userinfo' => {{'uid' => UID}, {'fullName' => NAME},...}},
+          raise Errno::ECONNREFUSED if options.redis_options == 'disabled'
+          cache = options.redis_options.nil? ? Redis.new : Redis.new(options.redis_options)
+          unless @user = (cache.get options.uid)
             get_user
             if @user
-              # Set Redis object for the user, and expire after 24 hours
-              cache.set @options[:uid], @user.to_yaml
-              cache.expire @options[:uid], 1440
+              cache.set options.uid, @user.to_yaml
+              cache.expire options.uid, 1440
             end
           else
-            # We found our user in the cache, let's parse it into a Ruby object
             @user = YAML::load(@user)
           end
-        # If we can't connect to Redis...
         rescue Errno::ECONNREFUSED => e
           get_user
         end
         @user
       end
 
-      # Query for the user against CASPORT, return as nil or parsed object
       def get_user
-        return if @user # no extra http calls
-        url = URI.escape("#{@options[:cas_server]}/#{@options[:uid]}.#{@options[:format]}")
+        return if @user
+        url = URI.escape("#{options.cas_server}/#{options.uid}.#{options.format}")
         response = Casport.get(url)
         if response.success?
           @user = response.parsed_response
--- oa-casport.gemspec
+++ oa-casport.gemspec
@@ -5,9 +5,9 @@ require "oa-casport/version"
 Gem::Specification.new do |s|
   s.name        = "oa-casport"
   s.version     = OmniAuth::Casport::VERSION
-  s.authors     = ["Jesus Jackson", "Steven Haddox"]
-  s.email       = ["jesusejackson@gmail.com", "stevenhaddox@shortmail.com"]
-  s.homepage    = "https://github.com/stevenhaddox/oa-casport"
+  s.authors     = ["Chase Pollock"]
+  s.email       = ["umdstu@gmail.com"]
+  s.homepage    = "https://github.com/umdstu/oa-casport"
   s.summary     = %q{OmniAuth gem for internal casport server}
   s.description = %q{ Simple gem to enable rack powered Ruby apps to authenticate internally via casport with ease}
   s.rubyforge_project = "oa-casport"
@@ -17,7 +17,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_dependency 'oa-core'
+  s.add_dependency 'omniauth', '~> 1.1.0'
   s.add_dependency 'httparty'
   s.add_dependency 'redis'
 
